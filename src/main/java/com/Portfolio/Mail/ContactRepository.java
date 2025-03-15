e6ed7ad02c06af8179f4141db6c200f3df839f9d
divya2111 <2000031047@kluniversity.in>
2025-03-15 14:09:04 +0530

    updated application properties

--- src/main/java/com/Portfolio/Mail/ContactRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Portfolio.Mail;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.Portfolio.Mail.ContactRequest;
-
-public interface ContactRepository extends JpaRepository<ContactRequest, Long> {
-}
--- src/main/java/com/Portfolio/Mail/ContactRequest.java
+++ src/main/java/com/Portfolio/Mail/ContactRequest.java
@@ -1,23 +1,22 @@
 package com.Portfolio.Mail;
 
-import jakarta.persistence.*;
+
 import lombok.*;
 
-@Entity
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 public class ContactRequest {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
     private String name;
     private String email;
-    @Column(length = 1000) // Store longer messages
+
     private String message;
-    @Temporal(TemporalType.TIMESTAMP)
+
     private java.util.Date submittedAt = new java.util.Date();
 
 }
